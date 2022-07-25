624ba54a09e249e2709a655d8a99fcf12b1dcae4
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-25 12:01:56 -0700

    Less Operator

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -326,3 +326,16 @@ function testGreaterOrEqual(val){
 }
 
 console.log(testGreaterOrEqual(10));
+
+function testLesserThan(val){
+    if (val < 25){
+        return "Under 25";
+    }
+    if (val < 55){
+        return "Under 55";
+    }
+    return "55 or over";
+}
+
+console.log(testLesserThan(10));
+
