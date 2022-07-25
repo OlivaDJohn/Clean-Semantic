3fcf2d889235d3a36e94501a30552ef8e03431c3
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-25 12:49:40 -0700

    Logical Order

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -393,3 +393,13 @@ function testElseIf(val){
 
 console.log(testElseIf(7));
 
+function orderMyLogic(val){
+    if (val < 10){
+        return "Less than 10";
+    } else if(val < 5){
+        return "Less than 5";
+    }
+    return "Greater than or equal to 10";
+}
+
+console.log(orderMyLogic(3));
