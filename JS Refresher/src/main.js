2e6b6173acdd56f796cfeac24f5e9af96acabb55
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-25 11:57:47 -0700

    Greater Operator

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -302,3 +302,16 @@ function testStrictInequality(val){
 }
 
 console.log(testStrictInequality(10));
+
+function testGreaterThan(val){
+    if (val > 100){
+        return "Over 100";
+    }
+    if (val > 10){
+        return "Over 10";
+    }
+    return "10 or under";
+}
+
+console.log(testGreaterThan(10));
+
