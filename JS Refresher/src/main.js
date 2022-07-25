062228aa19ac4a686d126761820a3d5c1e8d151f
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-25 13:47:55 -0700

    Return Early Pattern for Functions

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -533,11 +533,17 @@ function chainToSwitch(val){
 }
 
 function isLess(a,b){
-    if (a < b){
-        return true;
-    }
-    return false;
+    return a < b;
 }
 
 console.log(isLess(10,15));
 
+function abTest(a,b){
+    if(a < 0 || b < 0){
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2))
+}
+
+console.log(abTest(2,2));
