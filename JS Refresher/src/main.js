029a7c5a071a648e42c6b5ac5a49b08c0b5a5f02
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-25 13:17:19 -0700

    Default switch

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -462,3 +462,25 @@ function caseInSwitch(val){
 }
 
 console.log(caseInSwitch(2));
+
+function switchOfStuff(val){
+    let answer = "";
+    switch (val){
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+            break;
+    }
+    return answer;
+}
+
+console.log(switchOfStuff(5));
+
