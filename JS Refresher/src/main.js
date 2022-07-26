aaeceb21e9387bddf5495dcb9128c417c71578f9
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 11:58:09 -0700

    Destructuring assignment to pass an object as a functions parameter

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -1048,3 +1048,17 @@ function removeFirstTwo(list){
     return arr;
 }
 
+const stats = {
+    max:56.78,
+    standard_deviation:4.34,
+    median:34.54,
+    mode:23.87,
+    min:-0.75,
+    average: 35.85
+};
+
+const half = (function(){
+    return function half({ max, min}){
+        return (max + min) / 2.0;
+    };
+});
