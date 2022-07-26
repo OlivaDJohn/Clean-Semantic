559aad10648a23b1eb6d2069203456bf48a2176c
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 09:52:32 -0700

    Nested For Loops

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -799,3 +799,18 @@ for (let i = 0; i < ourArray.length; i++){
 
 console.log(ourTotal);
 
+function multiplyAll(arr){
+    var product = 1;
+
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; i < arr[i].length; j++){
+            product *= arr[i][j]
+        }
+    }
+
+    return product;
+}
+
+var product = multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+console.log(product);
