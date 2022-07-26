4220c6bc4efc717c2bde20507157c534b58ec70a
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 10:55:53 -0700

    Spread Operator

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -997,3 +997,10 @@ const sum = (function(){
         return args.reduce((a,b) => a + b, 0);
     };
 });
+
+const arr1 = ["JAN","FEB","MAR","APR","MAY"];
+let arr2;
+(function(){
+    arr2 = [...arr1];
+    arr1[0] = 'potato'
+})();
