8698271b47269b9578d8bbfc2933830837481d0a
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 10:27:59 -0700

    Var vs Let

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -920,3 +920,16 @@ function catTalk(){
 }
 
 catTalk();
+
+function checkScope(){
+    "use strict";
+    let i = "function scope";
+    if (true) {
+        let i = "block scope";
+        console.log("Block scope i is: ",i);
+    }
+    console.log("Block scope i is: ",i);
+    return i;
+}
+
+checkScope();
