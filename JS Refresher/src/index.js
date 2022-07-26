57d2f570dad7349776bc1badcc360d679cdefa7e
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 12:35:16 -0700

    Imports and Exports

---
+++ JS Refresher/src/index.js
@@ -0,0 +1,16 @@
+import { capitalizeString } from "./string_function";
+
+
+const cap  =capitalizeString("hello!");
+console.log(cap);
+
+const capitalizeString1 = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export{capitalizeString1 };
+
+export const foo = "bar";
+export const bar = "foo";
+
+import * as capitalizeString from "./string_function";
