23059ace78cc62e488ac713ae514c95e0ea556a3
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 09:08:46 -0700

    Object for Lookups

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -611,3 +611,40 @@ myDog.name = "Yodeling";
 myDog.bark = "Bow-wow";
 
 delete myDog.bark;
+
+function phoneticLookup(val){
+    let result = "";
+    let lookup = {
+        "alpha":"Adams",
+        "bravo":"Boston",
+        "charlie": "Chicago",
+        "delta":"Denver",
+        "echo":"Easy",
+        "foxtrot":"Frank"
+    };
+    result = lookup[val];
+    /*switch(val){
+        case "alpha":
+            result = "Adams";
+            break;
+        case "bravo":
+            result = "Boston";
+            break;
+        case "charlie":
+            result = "Chicago";
+            break;
+        case "delta":
+            result = "Denver";
+            break;
+        case "echo":
+            result = "Easy";
+            break;
+        case "foxtrot":
+            result = "Frank";
+            break;
+    }*/
+    return result;
+}
+
+console.log(phoneticLookup("charlie"));
+
