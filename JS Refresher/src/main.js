1c73d0e8f4e732adbebca36d304c38e1647864a4
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-26 10:52:43 -0700

    Rest Operators

--- JS Refresher/src/main.js
+++ JS Refresher/src/main.js
@@ -991,3 +991,9 @@ const increment = (function() {
         return number + value;
     };
 });
+
+const sum = (function(){
+    return function sum(...args){
+        return args.reduce((a,b) => a + b, 0);
+    };
+});
