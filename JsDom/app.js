7faccc7a22a1718fa3d32ed686da102572797b04
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-27 08:51:23 -0700

    Nodes

--- JsDom/app.js
+++ JsDom/app.js
@@ -3,7 +3,7 @@
 
 Array.from(titles).forEach(element => {
     console.log(element);
-});*/
+});
 //const WMF = document.querySelector('#book-list li:nth-child(2) .name');
 
 //console.log(WMF.outerHTML);
@@ -18,4 +18,13 @@ books.forEach(item => {
 
 const bookList = document.querySelector('#book-list');
 //bookList.innerHTML = '<h2>Books and more...</h2>';
-bookList.innerHTML += "<p>This is how you add HTML</p>"
+bookList.innerHTML += "<p>This is how you add HTML</p>"*/
+
+const banner = document.querySelector('#page-banner');
+
+console.log(`#page-banner node type is: ${banner.nodeType}`);
+console.log(`#page-banner node name is: ${banner.nodeName}`);
+console.log(`#page-banner has child nodes: ${banner.hasChildNodes()}`);
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner.outerHTML);
