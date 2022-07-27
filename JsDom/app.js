a7405def7c6c455e70efda8add3f88302d65ca8d
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-27 09:09:24 -0700

    Traversing the DOM

--- JsDom/app.js
+++ JsDom/app.js
@@ -31,7 +31,15 @@ console.log(clonedBanner.outerHTML);*/
 
 const bookList = document.querySelector('#book-list');
 
-console.log(`The parent node is: ${bookList.parentNode}`);
+/*console.log(`The parent node is: ${bookList.parentNode}`);
 console.log(`The parent node is: ${bookList.parentElement.parentElement}`);
 
-console.log(bookList.children);
+console.log(bookList.children);*/
+
+console.log(`#book-list next sibling is: ${bookList.nextSibling}`);
+console.log(`#book-list next element sibling is: ${bookList.nextElementSibling}`);
+
+console.log(`#book-list previous sibling is: ${bookList.previousSibling}`);
+console.log(`#book-list previous element sibling is: ${bookList.previousElementSibling}`);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else'
