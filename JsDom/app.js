fb5b23e0d403f9957e949ef385dec05e00c69ec8
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-27 09:50:59 -0700

    Events

--- JsDom/app.js
+++ JsDom/app.js
@@ -27,14 +27,14 @@ console.log(`#page-banner node name is: ${banner.nodeName}`);
 console.log(`#page-banner has child nodes: ${banner.hasChildNodes()}`);
 
 const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner.outerHTML);*/
+console.log(clonedBanner.outerHTML);
 
 const bookList = document.querySelector('#book-list');
 
-/*console.log(`The parent node is: ${bookList.parentNode}`);
+console.log(`The parent node is: ${bookList.parentNode}`);
 console.log(`The parent node is: ${bookList.parentElement.parentElement}`);
 
-console.log(bookList.children);*/
+console.log(bookList.children);
 
 console.log(`#book-list next sibling is: ${bookList.nextSibling}`);
 console.log(`#book-list next element sibling is: ${bookList.nextElementSibling}`);
@@ -42,4 +42,28 @@ console.log(`#book-list next element sibling is: ${bookList.nextElementSibling}`
 console.log(`#book-list previous sibling is: ${bookList.previousSibling}`);
 console.log(`#book-list previous element sibling is: ${bookList.previousElementSibling}`);
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else'
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else'*/
+
+const h2 = document.querySelector("#book-list h2");
+
+h2.addEventListener('click', (e) =>{
+    console.log(e.target);
+    console.log(e);
+})
+
+const buttons = document.querySelectorAll('#book-list .delete');
+
+buttons.forEach(item =>{
+    item.addEventListener('click', (e) =>{
+        const li = e.target.parentElement;
+
+        li.parentNode.removeChild(li);
+    })
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', (e) =>{
+    e.preventDefault();
+    console.log(`Navigation to ${e.target.textContent} was prevented`);
+})
