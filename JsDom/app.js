ef9be483a8dacea34bf53d265c7329f918220f21
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-27 08:32:29 -0700

    Query Selector

--- JsDom/app.js
+++ JsDom/app.js
@@ -1,6 +1,16 @@
-var titles = document.getElementsByClassName('title');
+/*var titles = document.getElementsByClassName('title');
 
 
 Array.from(titles).forEach(element => {
     console.log(element);
+});*/
+const WMF = document.querySelector('#book-list li:nth-child(2) .name');
+
+//console.log(WMF.outerHTML);
+
+const books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(item => {
+    console.log(item.outerHTML);
 });
