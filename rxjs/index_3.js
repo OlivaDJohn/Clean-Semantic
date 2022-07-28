c5576eafc9c673af39793fd242b5cce95e5b65fc
Oliva Dominguez <jonathan.oliva.external@atos.net>
2022-07-28 14:11:49 -0700

    Subject

--- rxjs/index_3.js
+++ rxjs/index_3.js
@@ -1,4 +1,4 @@
-import { of } from "rxjs";
+import { of, Subject } from "rxjs";
 import {scan, delay} from "rxjs/operators";
 
 const src = of(1,2,3,4,5).pipe(delay(1000));
@@ -7,3 +7,14 @@ const scanObs = src.pipe(
 );
 
 scanObs.subscribe(v => console.log(v));
+
+const subject = new Subject();
+subject.subscribe({
+    next: (n, x) => console.log(`ObsA: ${n}`)
+});
+subject.subscribe({
+    next: (n, x) => console.log(`ObsB: ${n + 1}`)
+});
+
+subject.next(1);
+subject.next(2);
