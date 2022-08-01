02899ec2a53f0303882560d772fc98375d7bfca7
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-08-01 14:04:58 -0700

    Webpack
    Commit done after the previous commit, which served as a backup in case something went wrong

--- webpack_1/src/index.js
+++ webpack_1/src/index.js
@@ -1,5 +1,11 @@
 import generateJoke from "./generateJoke";
 import './styles/main.scss';
+import laughing from './assets/laughing.svg';
 
-console.log(generateJoke());
-console.log(1);
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
--- webpack_1/webpack.config.js
+++ webpack_1/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'development',
@@ -10,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: '[name][ext]' 
     },
     devServer: {
         static: {
@@ -32,6 +34,20 @@ module.exports = {
                     'sass-loader',
                 ],
             },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
         ],
     },
     plugins: [
@@ -39,6 +55,7 @@ module.exports = {
             title: 'Webpack App',
             filename: 'index.html',
             template: 'src/template.html',
-        })
+        }),
+        new BundleAnalyzerPlugin(),
     ]
 }
