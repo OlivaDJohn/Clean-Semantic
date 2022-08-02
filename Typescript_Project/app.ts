930446fca8a617530018f18f253f8e1ca9aeea3d
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-08-02 13:53:33 -0700

    Union Types

--- Typescript_Project/app.ts
+++ Typescript_Project/app.ts
@@ -1,48 +1,16 @@
-/*const person: {
-    name: string;
-    age: number;
+function combine (input1: number | string, input2: number | string){
+    let result;
+    if (typeof input1 === 'number'&& typeof input2 === 'number'){
+        result = input1 + input2;
+    } else{
+        result = input1.toString() + input2.toString();
+    }
 
-}*/ 
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-// } = {
-//     name: "Jonathan",
-//     age: 20,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR'};
-
-const person = {
-    name: "Jonathan",
-    age: 20,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-
-//person.role.push('admin');
-//person.role[1] = 10;
-
-//person.role = [0, 'admin', 'user']
-
-let favoriteActivities: any[];//AVOID AT ALL COS
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies){
-    console.log(hobby.toUpperCase());
+    return result;
 }
 
-if (person.role === Role.AUTHOR){
-    console.log('is author...');
-    
-}
+const combinedAges = combine(30,26);
+console.log(combinedAges);
+
+const combinedNames = combine("Ban","Anna");
+console.log(combinedNames);
