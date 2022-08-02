0d688ffbe2446869b9e25fb3646e13166e165702
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-08-02 13:20:16 -0700

    Object Types

--- Typescript_Project/app.ts
+++ Typescript_Project/app.ts
@@ -1,21 +1,9 @@
-function add (n1: number, n2: number, showResult: boolean, phrase: string){
-    /*
-    JavaScript
-    if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-        throw new Error('Incorrect input!');
-    }*/
-    let result = n1 + n2;
-    if (showResult){
-       console.log(phrase + result);
-    }else {
-        return result;
-    }
-}
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Jonathan",
+    age: 20
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhase = 'Result is: ';
-
-const result = add(number1, number2, printResult, resultPhase);
-
+console.log(person.name);
