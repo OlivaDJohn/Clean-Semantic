1d3b14a0c1ddc8b0e97e8df0b30400d90403f5b4
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-08-02 13:25:36 -0700

    Array Types

--- Typescript_Project/app.js
+++ Typescript_Project/app.js
@@ -1,17 +1,5 @@
-function add(n1, n2, showResult) {
-    /*
-    JavaScript
-    if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-        throw new Error('Incorrect input!');
-    }*/
-    if (showResult) {
-        console.log(n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var result = add(number1, number2, printResult);
+var person = {
+    name: "Jonathan",
+    age: 20
+};
+console.log(person.name);
--- Typescript_Project/app.ts
+++ Typescript_Project/app.ts
@@ -1,9 +1,19 @@
-const person: {
+/*const person: {
     name: string;
     age: number;
-} = {
+
+}*/ 
+const person = {
     name: "Jonathan",
-    age: 20
+    age: 20,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
